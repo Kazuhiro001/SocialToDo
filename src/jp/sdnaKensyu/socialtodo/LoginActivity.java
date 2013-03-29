39b1ee68d3decd3246b7a98e23ee5ca213fcee99
seiji kato <musuka_hair@yahoo.ac.jp>
2013-03-30 01:46:16 +0900

    ログイン保持の範囲確認を行えるよう変更。

--- src/jp/sdnaKensyu/socialtodo/LoginActivity.java
+++ src/jp/sdnaKensyu/socialtodo/LoginActivity.java
@@ -26,22 +26,19 @@ public class LoginActivity extends Activity{
 		setContentView(R.layout.activity_login);
 		Button button = (Button) findViewById(id.buttonForLogin);
 	    final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-		alertDialogBuilder.setTitle("警告");
+		alertDialogBuilder.setTitle("応答コード確認");
 		alertDialogBuilder.setPositiveButton("確認",
 				new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					Intent intent = new Intent(LoginActivity.this,
-							 MainActivity.class);
-					 startActivity(intent);
 				}
 			});
 		alertDialogBuilder.setCancelable(true);
-		
+
 		 button.setOnClickListener(new View.OnClickListener() {
 			 @Override
 			 public void onClick(View v) {
-				 URI uri = null;
+				URI uri = null;
 				HttpClient http;
 				http = new DefaultHttpClient();
 				HttpParams params = http.getParams();
@@ -56,12 +53,50 @@ public class LoginActivity extends Activity{
 				try{
 				    HttpGet objGet   = new HttpGet(uri);
 				    HttpResponse response = http.execute(objGet);
-					alertDialogBuilder.setMessage("コード確認：" + response.getStatusLine().getStatusCode() + "\nメイン画面に戻ります。");
+					alertDialogBuilder.setMessage("コード確認：" + response.getStatusLine().getStatusCode());
 				    AlertDialog alertDialog = alertDialogBuilder.create();
 				    alertDialog.show();
 				}catch(IOException e){
 				}
 			}
 		});
+
+		button = (Button) findViewById(id.buttonForCheck);
+			button.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					URI uri = null;
+					HttpClient http;
+					http = new DefaultHttpClient();
+					HttpParams params = http.getParams();
+					HttpConnectionParams.setConnectionTimeout(params, 1000); //接続のタイムアウト
+					HttpConnectionParams.setSoTimeout(params, 1000); //データ取得のタイムアウト
+					try{
+						uri = new URI("http://yoshio916.s349.xrea.com/api/v1/GetUserInformation/");
+					}catch(URISyntaxException e ){
+					}
+					try{
+					    HttpGet objGet   = new HttpGet(uri);
+					    HttpResponse response = http.execute(objGet);
+						alertDialogBuilder.setMessage("コード確認：" + response.getStatusLine().getStatusCode());
+					    AlertDialog alertDialog = alertDialogBuilder.create();
+					    alertDialog.show();
+					}catch(IOException e){
+					}
+			}
+		});
+
+		button = (Button) findViewById(id.buttonToMain);
+			button.setOnClickListener(new View.OnClickListener() {
+				 @Override
+				 public void onClick(View v) {
+					 // ボタンがクリックされた時に呼び出されます
+					 Intent intent = new Intent(LoginActivity.this,
+							 MainActivity.class);
+					 startActivity(intent);
+				}
+			});
+
+
 	}
 }

7b7e8f86af6c0ad56ffb26e47f2a5fbf1c56e374
kazuhiro <ooi.kazuhiro@gmail.com>
2013-03-30 19:24:39 +0900

    タスク表示画面のレイアウト関係変更。
    グループ選択とメンバー選択用にスピナー追加。

--- src/jp/sdnaKensyu/socialtodo/PlotActivity.java
+++ src/jp/sdnaKensyu/socialtodo/PlotActivity.java
@@ -1,8 +1,11 @@
 package jp.sdnaKensyu.socialtodo;
 
+import jp.sdnaKensyu.socialtodo.R.id;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PlotActivity extends Activity {
 	PlotView mPlotView;
@@ -11,6 +14,15 @@ public class PlotActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_plot);
+
+		///グループ選択用
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		adapter.add("test");
+		Spinner spinner = (Spinner) findViewById(id.spinner_group);
+		spinner.setAdapter(adapter);
+
+
 		mPlotView = (PlotView) findViewById(R.id.plotView1);
 	}
 
